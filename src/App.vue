72f266ae55b5f9b1b38cc88c871f967e6b701b23
Jurphy <1533179658@qq.com>
2022-07-28 12:50:33 +0800

    配置less

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    黑马头条
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <span class="toutiao toutiao-youjiantou"></span>
+    <div class="box"></div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style lang="less">
-
+<style lang="less" scoped>
+  .box {
+    background-color: pink;
+    width: 200px;
+    height: 100px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入iconfont.css
+import '@/assets/fonts/iconfont.css'
+// 引入Vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 引入amfe-flexible
+import 'amfe-flexible/index.min.js'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
